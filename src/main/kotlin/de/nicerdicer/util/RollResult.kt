1ebb3471c309f30a2df640696beb4a6bd6ab05ca
Runebreaker <gino.schulze-skyfire@web.de>
2026-06-15 23:44:46 +0200

    Add new RollResult to handle dice rolls and tracking

---
+++ src/main/kotlin/de/nicerdicer/util/RollResult.kt
@@ -0,0 +1,51 @@
+package de.nicerdicer.util
+
+import dev.kord.core.behavior.interaction.response.DeferredPublicMessageInteractionResponseBehavior
+import dev.kord.core.behavior.interaction.response.respond
+
+class RollResult(val diceType: Int, val amount: Int, val modifier: Int)
+{
+    private val diceRolls: MutableList<Int> = mutableListOf()
+    private var result: Int = 0
+    private var isCrit: Boolean = false
+
+    /**
+     * Rolls the dice new and sets the result and isCrit. Returns true, if successful or false otherwise.
+     */
+    fun roll(): Boolean
+    {
+        if (diceType <= 0 || amount <= 0) return false
+
+        diceRolls.clear()
+        for (i in 1..amount)
+        {
+            val roll = NicerRandom.nextRoll(diceType)
+            diceRolls.add(roll)
+            if (roll == diceType) isCrit = true
+        }
+        result = diceRolls.max() + modifier
+
+        return true
+    }
+
+    fun getDiceRolls(): List<Int> = diceRolls.toList()
+
+    fun getResult(): Int = result
+
+    fun isCrit() = isCrit
+
+    fun getRollString(): String = "${getInputString()} => ${toString()}"
+
+    fun getInputString(): String = "$amount D$diceType ${if (modifier >= 0) "+" else "-"} $modifier"
+
+    override fun toString(): String
+    {
+        val sb = StringBuilder()
+
+        sb.append(StringFormatter.formatRolls(diceRolls))
+        sb.append(" (+ $modifier) = ")
+        sb.append(StringFormatter.formatResult(result, isCrit))
+
+        return sb.toString()
+    }
+}
