d471b1c9307603fc61a9d5dc8b2f4f76f1ee60ec
Runebreaker <gino.schulze-skyfire@web.de>
2026-06-15 23:45:43 +0200

    Add a string formatter and some extensions

---
+++ src/main/kotlin/de/nicerdicer/util/StringFormatter.kt
@@ -0,0 +1,17 @@
+package de.nicerdicer.util
+
+object StringFormatter
+{
+    fun formatRolls(rolls: List<Int>): String = rolls.joinToString(" + ") { if (it == rolls.max()) it.toString() else it.toString().stricken() }
+
+    fun formatResult(result: Int, isCrit: Boolean): String
+    {
+        if (isCrit) return result.toString().bold().underscored()
+        return result.toString()
+    }
+}
+
+fun String.bold(): String = "**$this**"
+fun String.italic(): String = "*$this*"
+fun String.underscored(): String = "__${this}__"
+fun String.stricken(): String = "~~${this}~~"
